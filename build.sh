7e9c3b4fa630ce2069918ea2ff3ac8719b0bf92d
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2015-09-30 14:54:57 +0200

    Restructure books, complete book.

--- build.sh
+++ build.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
 # Build the books
-asciidoctor -b html -o build/proteomics.html book-proteomics.adoc
-asciidoctor -b html -o build/metabolomics.html book-metabolomics.adoc
+asciidoctor -b html -o build/proteomics.html proteomics.adoc
+asciidoctor -b html -o build/metabolomics.html metabolomics.adoc
+
+asciidoctor -b html -o build/the-python-lab-book.html complete.adoc
 
 # Build the PDFs
-asciidoctor -b pdf -o build/proteomics.pdf book-proteomics.adoc
-asciidoctor -b pdf -o build/metabolomics.pdf book-metabolomics.adoc
+asciidoctor -b pdf -o build/proteomics.pdf proteomics.adoc
+asciidoctor -b pdf -o build/metabolomics.pdf metabolomics.adoc
+
+asciidoctor -b pdf -o build/the-python-lab-book.pdf complete.adoc
 
 # Copy the images into location
 cp -r ./img ./build/img
+
+cp -r ./covers ./build/img
+cp -r ./www ./build
