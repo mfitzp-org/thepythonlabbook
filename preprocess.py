ee544f26817ab34c685b150094c405fa4480c1d9
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2015-11-02 10:34:55 +0100

    Auto-generate and delete folders for figure outputs

--- preprocess.py
+++ preprocess.py
@@ -39,9 +39,15 @@ runner = NotebookRunner(None)
 # Iterate list of documents; searching for code blocks and snippets
 # using [source,python,  (exec,stdout)
 for file in adocs:
-
     blocks = []
-    print(file)
+    
+    # Make output folder for images
+    folder = os.path.join('.','img',os.path.splitext(os.path.basename(file))[0])
+    try:
+        os.mkdir(folder)
+    except OSError:
+        pass
+    
     with open(file, 'r') as f:
         txt = f.read()
 
@@ -54,63 +60,67 @@ for file in adocs:
         for mo in rx.finditer(txt):
             matches.append( (mo.span(), mo) )
 
-    if not matches:
-        continue
+    if matches:
 
-    matches.sort(key=lambda x: x[0][0])
+        matches.sort(key=lambda x: x[0][0])
 
-    for (start, end), mo in matches:
-        changes += 1
-        cell = NotebookNode(
-            cell_type='code',
-            input=mo.group('code'),
-            metadata={},
-            #span=mo.span(),
-        )
-        blocks.append(cell)
-        try:
-            runner.run_cell(cell)
+        for (start, end), mo in matches:
+            changes += 1
+            cell = NotebookNode(
+                cell_type='code',
+                input=mo.group('code'),
+                metadata={},
+                #span=mo.span(),
+            )
+            blocks.append(cell)
+            try:
+                runner.run_cell(cell)
 
-        except Exception as e:
-            pass
+            except Exception as e:
+                pass
 
-        output = []
-        for out in cell.outputs:
-            if hasattr(out, 'text'):
-                output.append( out.text )
-            if hasattr(out, 'traceback'):
-                output.append( ansi_escape.sub('', '\n'.join(out.traceback)))
+            output = []
+            for out in cell.outputs:
+                if hasattr(out, 'text'):
+                    output.append( out.text )
+                if hasattr(out, 'traceback'):
+                    output.append( ansi_escape.sub('', '\n'.join(out.traceback)))
 
-        if not output:
-            continue
+            if not output:
+                continue
 
-        # Join up then strip trailing newlines.
-        output = '\n'.join(output)
-        output = output.strip()
+            # Join up then strip trailing newlines.
+            output = '\n'.join(output)
+            output = output.strip()
 
-        # We want to output the relevant result, whether
-        # stdout or stderr. Output is to the subsequent
-        # '....' delimited block (pre).
+            # We want to output the relevant result, whether
+            # stdout or stderr. Output is to the subsequent
+            # '....' delimited block (pre).
 
-        start, end = start+offset, end+offset
+            start, end = start+offset, end+offset
 
-        # Only fill in blocks immediately followed by a container
-        if output and txt[end:end+len(STDOUT_DELIMITER)] == STDOUT_DELIMITER:
-            outstart = end+len(STDOUT_DELIMITER)
-            outend = txt[end+len(STDOUT_DELIMITER):].index(STDOUT_DELIMITER)+outstart
-            previous_len = len(txt)
-            txt = txt[:outstart+1] + output + txt[outend:]
-            offset += len(txt) - previous_len
-        else:
-            if output:
-                # If there is nowhere for the output to go; display
-                print("Unused output from:")
-                print(mo.group('code'))
-                print("-------------")
-                print(output)
-                print("-------------")
+            # Only fill in blocks immediately followed by a container
+            if output and txt[end:end+len(STDOUT_DELIMITER)] == STDOUT_DELIMITER:
+                outstart = end+len(STDOUT_DELIMITER)
+                outend = txt[end+len(STDOUT_DELIMITER):].index(STDOUT_DELIMITER)+outstart
+                previous_len = len(txt)
+                txt = txt[:outstart+1] + output + txt[outend:]
+                offset += len(txt) - previous_len
+            else:
+                if output:
+                    # If there is nowhere for the output to go; display
+                    print("Unused output from:")
+                    print(mo.group('code'))
+                    print("-------------")
+                    print(output)
+                    print("-------------")
 
-    # If we matched anything we need to rewrite
-    if changes:
-        with open(file, 'w') as f:
-            f.write(txt)
+        # If we matched anything we need to rewrite
+        if changes:
+            with open(file, 'w') as f:
+                f.write(txt)
+
+    # Clean up empty folder
+    if os.listdir(folder) == []:
+        print("DELETING", folder)
+        os.rmdir(folder)
